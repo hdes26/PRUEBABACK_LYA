ccb388ba2ce52b1c813e15ac8e9312da205715d9
Hernan Escorcia <hdes.2000@hotmail.com>
2022-08-26 20:49:04 -0500

    conexion base de datos

--- models/server.js
+++ models/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const { dbConnection } = require("../database/config");
 
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
+
+    //Conectar a base de datos
+    this.conectarDB();
+
+    //Middlewares
+    this.middlewares();
+  }
+
+  async conectarDB() {
+    await dbConnection();
   }
 
   middlewares() {
